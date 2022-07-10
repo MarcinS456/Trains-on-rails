4c7209a0eadcc86e2eca9109e68e76816d3c3fe9
Tomek Lasica <tomasz.lasica@datastax.com>
2022-07-10 12:29:06 +0200

    Pair programming to improve code readability

--- PKP_SCRAP.py
+++ PKP_SCRAP.py
@@ -1,100 +1,124 @@
-import pandas as pd
-from datetime import date, timedelta
-import selenium
-from get_gecko_driver import GetGeckoDriver
-from selenium import webdriver
-import time
 import os
-import requests
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import time
+from datetime import date, timedelta
+
+import pandas as pd
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
 
+
 def check_exists_by_xpath(xpath):
     try:
-        driver.find_element(By.XPATH,xpath)
+        driver.find_element(By.XPATH, xpath)
     except NoSuchElementException:
         return False
     return True
 
-options.headless = True
+
+def wait_for_the_page_to_load():
+    time.sleep(2)
+
+
+def sleep_to_not_stress_server():
+    time.sleep(1)
+
+
+options.headless = False
+
 driver = webdriver.Firefox(options=options)
 driver.set_window_size(1920, 1080)
 actions = ActionChains(driver)
 
-today=date.today().strftime("%d.%m.%y")
-yesterday=date.today()+timedelta(days=-1)
-yesterday=yesterday.strftime("%d.%m.%y")
+today = date.today().strftime("%d.%m.%y")
+yesterday = date.today() + timedelta(days=-1)
+yesterday = yesterday.strftime("%d.%m.%y")
 
-Trains=pd.DataFrame()
-counter=0
-path=os.getcwd()
-cities=pd.read_csv(path+'/cities.csv')
+Trains = pd.DataFrame()
+path = os.getcwd()
+cities = pd.read_csv(path + '/cities.csv')
 cities_list = cities['City'].to_list()
-i=1
-for origination in cities_list:
-    for destination in cities_list:
-        if origination!=destination:
-            check_date = 0
-            search_url = 'https://rozklad-pkp.pl/'
-            driver.get(search_url)
-            time.sleep(2)
-            while check_exists_by_xpath('//button[@class=" css-1hy2vtq"]') == True:
-                cookie_button = driver.find_element(By.XPATH, '//button[@class=" css-1hy2vtq"]')
-                cookie_button.click()
-            time.sleep(1)
-            if check_exists_by_xpath('//div[@class="qc-cmp2-buttons-desktop"]') == True:
-                cookie_button = driver.find_element(By.XPATH, '//div[@class="qc-cmp2-buttons-desktop"]')
-                cookie_button.click()
-            time.sleep(2)
-            print("count "+origination+" "+destination+ "route")
-            orig = driver.find_element(By.XPATH, '//input[@id="from-station"]')
-            time.sleep(1)
-            dest= driver.find_element(By.XPATH, '//input[@id="to-station"]')
-            orig.send_keys(origination)
-            time.sleep(1)
-            dest.send_keys(destination)
-            time.sleep(2)
-            depart = driver.find_element(By.XPATH, '//input[@id="hour"]')
-            depart.send_keys("00:00")
-            time.sleep(2)
-            direct_train = driver.find_element(By.XPATH, '//div[@class="checkbox3"]')
-            ActionChains(driver).move_to_element(direct_train).perform()
-            time.sleep(1)
-            direct_train.click()
-            time.sleep(1)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            submit = driver.find_element(By.XPATH, '//img[@class="search-icon"]')
-            ActionChains(driver).move_to_element(submit).perform()
-            time.sleep(1)
-            submit.click()
-            time.sleep(2)
 
-            while check_date!=-1:
-                counter = 0
-                if check_exists_by_xpath('//tr'):
-                    trains = driver.find_elements(By.XPATH, '//tr')
-                    for train in trains:
-                        txt=train.text
-                        check_date=txt.find(today)
-                        check_yesterday=txt.find(yesterday)
-                        if (check_yesterday==-1 and check_date!=-1):
-                            counter=counter+1
-                            #print(counter)
-                    time.sleep(1)
-                    if check_exists_by_xpath('//a[@class="tp later-connections"]'):
-                        next_trains = driver.find_element(By.XPATH, '//a[@class="tp later-connections"]')
-                        next_trains.click()
-                else: check_date=-1
-            if counter>0:
-                Trains.loc[i,'Origination']=origination
-                Trains.loc[i,'Destination']=destination
-                Trains.loc[i,'daily_trains']=counter
-            i=i+1
+timetable_search_url = 'https://rozklad-pkp.pl/'
+
+df_idx = 1
+
+for origination in cities_list:
+    other_cities = [c for c in cities_list if c != origination]
+    print(origination, "->", other_cities)
+    for destination in other_cities:
+        driver.get(timetable_search_url)
+        wait_for_the_page_to_load()
+
+        # close popup
+        while check_exists_by_xpath('//button[@class=" css-1hy2vtq"]'):
+            cookie_button = driver.find_element(By.XPATH, '//button[@class=" css-1hy2vtq"]')
+            cookie_button.click()
+        wait_for_the_page_to_load()
+
+        # close privacy settings popup
+        if check_exists_by_xpath('//div[@class="qc-cmp2-buttons-desktop"]'):
+            cookie_button = driver.find_element(By.XPATH, '//div[@class="qc-cmp2-buttons-desktop"]')
+            cookie_button.click()
+        wait_for_the_page_to_load()
+
+        print("count", origination, destination, "route")
+        orig = driver.find_element(By.XPATH, '//input[@id="from-station"]')
+        sleep_to_not_stress_server()
+
+        dest = driver.find_element(By.XPATH, '//input[@id="to-station"]')
+        orig.send_keys(origination)
+        sleep_to_not_stress_server()
+        dest.send_keys(destination)
+        sleep_to_not_stress_server()
+
+        depart = driver.find_element(By.XPATH, '//input[@id="hour"]')
+        depart.send_keys("00:00")
+        sleep_to_not_stress_server()
+
+        direct_train = driver.find_element(By.XPATH, '//div[@class="checkbox3"]')
+        ActionChains(driver).move_to_element(direct_train).perform()
+        sleep_to_not_stress_server()
+
+        direct_train.click()
+        sleep_to_not_stress_server()
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        submit = driver.find_element(By.XPATH, '//img[@class="search-icon"]')
+        ActionChains(driver).move_to_element(submit).perform()
+        sleep_to_not_stress_server()
+        submit.click()
+        wait_for_the_page_to_load()
+
+        is_date_today = False
+        counter = 0
+        while not is_date_today:
+            counter = 0
+            if check_exists_by_xpath('//tr'):
+                trains = driver.find_elements(By.XPATH, '//tr')
+                for train in trains:
+                    txt = train.text
+                    is_date_today = today in txt
+                    is_date_yesterday = yesterday in txt
+                    if not is_date_yesterday and not is_date_today:
+                        counter = counter + 1
+                        # print(counter)
+                sleep_to_not_stress_server()
+                if check_exists_by_xpath('//a[@class="tp later-connections"]'):
+                    next_trains = driver.find_element(By.XPATH, '//a[@class="tp later-connections"]')
+                    next_trains.click()
+            else:
+                is_date_today = False
+
+        if counter > 0:
+            Trains.loc[df_idx, 'Origination'] = origination
+            Trains.loc[df_idx, 'Destination'] = destination
+            Trains.loc[df_idx, 'daily_trains'] = counter
+        df_idx = df_idx + 1
 
 driver.quit()
-Trains.to_csv(path+'/graf.csv', index=False)
+Trains.to_csv(path + '/graf.csv', index=False)
